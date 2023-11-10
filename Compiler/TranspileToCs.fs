2bf020c6593db9364e2c48fdeb4fa02882df6455
Igor Gapanovich <gapanovich.igor@gmail.com>
2023-11-10 00:32:10 -0600

    Move function calls from expression blocks to preceding statements

--- Compiler/TranspileToCs.fs
+++ Compiler/TranspileToCs.fs
@@ -6,7 +6,7 @@ open Compiler.Type
 type private TypedExpression = Ast.TypedExpression<Type>
 
 type private EnclosingFunctionBodyContext =
-    { addVar: CsAst.Type -> CsAst.Identifier -> CsAst.Expression -> unit }
+    { addPrecedingStatement: CsAst.Statement -> unit }
 
 let private mapBinaryOperator (o: Ast.BinaryOperator) =
     match o with
@@ -15,12 +15,9 @@ let private mapBinaryOperator (o: Ast.BinaryOperator) =
     | Ast.BinaryOperator.Multiply -> CsAst.BinaryOperator.Multiply
     | Ast.BinaryOperator.Divide -> CsAst.BinaryOperator.Divide
 
-let private (|Is|_|) v1 v2 =
-    if v1 = v2
-    then Some ()
-    else None
+let private (|Is|_|) v1 v2 = if v1 = v2 then Some() else None
 
-let private mapType (t: Type): CsAst.Type =
+let private mapType (t: Type) : CsAst.Type =
     match t with
     | Is BuiltInTypes.int -> "System.Int32"
     | Is BuiltInTypes.float -> "System.Single"
@@ -28,92 +25,98 @@ let private mapType (t: Type): CsAst.Type =
     | Is BuiltInTypes.unit -> failwith "TODO handle void"
     | _ -> failwith "TODO handle other types"
 
-let private mapExpression (ctx: EnclosingFunctionBodyContext) (e: TypedExpression): CsAst.Expression option =
+let private mapExpression (ctx: EnclosingFunctionBodyContext) (e: TypedExpression) : CsAst.Expression option =
     match e.expression with
     | Ast.Identifier i -> CsAst.Identifier i |> Some
-    | Ast.NumberLiteral (i, f) ->
+    | Ast.NumberLiteral(i, f) ->
         let t = mapType e.expressionType
-        CsAst.NumberLiteral (i, f, t) |> Some
+        CsAst.NumberLiteral(i, f, t) |> Some
     | Ast.StringLiteral s -> CsAst.StringLiteral s |> Some
-    | Ast.BinaryOperation (e1, op, e2) ->
+    | Ast.BinaryOperation(e1, op, e2) ->
         let e1 = mapExpression ctx e1
         let op = mapBinaryOperator op
         let e2 = mapExpression ctx e2
+
         match e1, e2 with
-        | Some e1, Some e2 ->
-            CsAst.BinaryOperation (e1, op, e2)
-            |> Some
+        | Some e1, Some e2 -> CsAst.BinaryOperation(e1, op, e2) |> Some
         | _ -> failwith "Operands of a binary operation should not be statements"
-    | Ast.Application (f, arg) ->
+    | Ast.Application(f, arg) ->
         match f.expression with
         | Ast.Identifier i ->
             let arg = mapExpression ctx arg
+
             let args =
                 match arg with
-                | Some arg -> [arg]
+                | Some arg -> [ arg ]
                 | None -> []
 
-            CsAst.FunctionCall (i, args)
-            |> Some
+            CsAst.FunctionCall(i, args) |> Some
         | _ -> failwith "TODO handle case when function is not represented by an identifier"
-    | Ast.Coerce (e, t) ->
+    | Ast.Coerce(e, t) ->
         let e = mapExpression ctx e
         let t = mapType t
+
         match e with
-        | Some e ->
-            CsAst.Cast (e, t)
-            |> Some
+        | Some e -> CsAst.Cast(e, t) |> Some
         | None -> failwith "Coerce on a statement"
-    | Ast.Let (i, v) ->
+    | Ast.Let(i, v) ->
         let t = mapType v.expressionType
         let v = mapExpression ctx v
+
         match v with
-        | Some v -> ctx.addVar t i v
+        | Some v -> ctx.addPrecedingStatement (CsAst.Var(t, i, v))
         | None -> ()
+
         None
     | Ast.Sequence es ->
-        let es = es |> List.map (mapExpression ctx)
-        es |> List.last
+        let mutable es = es |> List.map (mapExpression ctx)
+        while es.Length > 1 do
+            let eHead = es.Head
+            match eHead with
+            | Some (CsAst.FunctionCall (f, args)) -> ctx.addPrecedingStatement (CsAst.Statement.FunctionCall(f, args))
+            | _ -> ()
+            es <- es.Tail
 
-let private mapStatement (ctx: EnclosingFunctionBodyContext) (e: TypedExpression): CsAst.Statement list =
+        es.Head
+
+let private mapStatement (ctx: EnclosingFunctionBodyContext) (e: TypedExpression) : CsAst.Statement list =
     match e.expression with
-    | Ast.Let (i, v) ->
+    | Ast.Let(i, v) ->
         let t = mapType v.expressionType
         let v = mapExpression ctx v
+
         match v with
-        | Some v -> [ CsAst.Statement.Var (t, i, v) ]
+        | Some v -> [ CsAst.Statement.Var(t, i, v) ]
         | None -> failwith "Let binding has statement as its value"
-    | Ast.Application (f, arg) ->
+    | Ast.Application(f, arg) ->
         match f.expression with
         | Ast.Identifier i ->
             let arg = mapExpression ctx arg
+
             let args =
                 match arg with
-                | Some arg -> [arg]
+                | Some arg -> [ arg ]
                 | None -> []
-            [ CsAst.Statement.FunctionCall (i, args) ]
+
+            [ CsAst.Statement.FunctionCall(i, args) ]
         | _ -> failwith "TODO handle case when function is not represented by an identifier"
-    | Ast.Sequence es ->
-        es |> List.collect (mapStatement ctx)
+    | Ast.Sequence es -> es |> List.collect (mapStatement ctx)
     | _ -> failwith "TODO handle other statements"
 
-let private mapFunctionBody (e: TypedExpression): CsAst.Statement list =
+let private mapFunctionBody (e: TypedExpression) : CsAst.Statement list =
     let statements = List()
 
-    let context =
-        { addVar = fun t i v ->
-            statements.Add(CsAst.Statement.Var (t, i, v)) }
+    let context = { addPrecedingStatement = statements.Add }
 
     match e.expression with
     | Ast.Sequence es ->
         for e in es do
             statements.AddRange(mapStatement context e)
-    | _ ->
-        statements.AddRange(mapStatement context e)
+    | _ -> statements.AddRange(mapStatement context e)
 
     List.ofSeq statements
 
-let transpile (ast: Ast.Program<Type>): CsAst.Program =
+let transpile (ast: Ast.Program<Type>) : CsAst.Program =
     let (Ast.Program e) = ast
 
     let statements = mapFunctionBody e
