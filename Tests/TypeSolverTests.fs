88f723a2287189dcf80c2eaec64a97ac1fa1e790
Igor Gapanovich <gapanovich.igor@gmail.com>
2024-01-07 14:38:32 -0600

    WIP

--- Tests/TypeSolverTests.fs
+++ Tests/TypeSolverTests.fs
@@ -9,6 +9,7 @@ open TypeSolver
 
 let private run (code: string) =
     let stream = new MemoryStream(Encoding.UTF8.GetBytes(code))
+
     match Parser.parseAst stream with
     | Ok ast -> Solver.getTypeInformation ast
     | Error _ -> failwith "Failed to parse"
@@ -16,11 +17,7 @@ let private run (code: string) =
 let private identifierType (identifier: string) (typeInfo: TypeInformation) =
     typeInfo.identifierTypes
     |> Map.toSeq
-    |> Seq.choose (fun (i, t) ->
-        if i.Name = identifier then
-            Some t
-        else
-            None)
+    |> Seq.choose (fun (i, t) -> if i.Name = identifier then Some t else None)
     |> Seq.exactlyOne
 
 let private assertIsAtom (atomTypeId: AtomTypeId) (t: Type) =
@@ -33,3 +30,13 @@ let valueBindingToLiteral () =
     let typeInfo = run "let x = 5"
 
     typeInfo |> identifierType "x" |> assertIsAtom BuiltIn.AtomTypeIds.int
+
+[<Test>]
+let valueBindingToIdentifier () =
+    let typeInfo =
+        "let x = 5\n\
+         let y = x"
+        |> run
+
+    typeInfo |> identifierType "x" |> assertIsAtom BuiltIn.AtomTypeIds.int
+    typeInfo |> identifierType "y" |> assertIsAtom BuiltIn.AtomTypeIds.int
